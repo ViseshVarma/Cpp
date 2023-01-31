e22435536548cea61787d81659b3c01995e2072a
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-02-01 04:29:09 +0530

    01-02-2023

---
+++ CODECHEF/1*-2* Learning Path/Basic Programming/SMOL.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n, k;
+	    cin >> n >> k;
+	    int ans = n - k;
+	    if(k == 0)
+	    {
+	        cout << n << endl;
+	    }
+	    else
+	    {
+	        cout << n%k << endl;
+	    }
+	}
+	
+	return 0;
+}
