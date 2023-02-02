6decaa4a5f62af6c1f4329ba2f4bf0913766cec8
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-02-02 09:46:56 +0530

    02-02-2023

---
+++ CODECHEF/1*-2* Learning Path/Time Complexity/MAXTHEMIN.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    int arr[n];
+	    for(int i=0; i<n; i++){
+	        cin >> arr[i];
+	    }
+	    sort(arr, arr+n);
+	    if(k >= n-1){
+	        cout << arr[n-1] <<endl;
+	    }
+	    else
+	    {
+	        cout << arr[k] << endl;
+	    }
+	}
+	
+	return 0;
+}
