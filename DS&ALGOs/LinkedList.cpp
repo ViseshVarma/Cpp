04dd6a227fef2d8bcd3930cb42cea909032ba8d7
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-02-03 17:02:36 +0530

    03-02-2023

--- DS&ALGOs/LinkedList.cpp
+++ DS&ALGOs/LinkedList.cpp
@@ -49,6 +49,85 @@ class LinkedList
         void getLength(){
             cout << "Length: " << length << endl;
         }
+
+        void append(int value){
+            Node* newNode = new Node(value);
+            if(length == 0){
+            head = newNode;
+            tail = newNode;
+            }
+            else{
+                tail->next = newNode;
+                tail = newNode;
+            }
+            length++;
+        }
+
+        void deleteLast() {
+            if(length == 0) return;
+            Node* temp = head;
+            Node* pre = head;
+            while(temp->next) {
+                pre = temp;
+                temp = temp->next;
+            }
+            tail = pre;
+            tail->next = nullptr;
+            length--;
+            if(length == 0){
+                head = nullptr;
+                tail = nullptr;
+            }
+            delete temp;
+        }
+
+        void prepend(int value){
+            Node* newNode = new Node(value);
+            if(length == 0){
+                head = newNode;
+                tail = newNode;
+            }
+            else{
+                newNode -> next = head;
+                head = newNode;
+            }
+            length++;
+        }
+
+        void deleteFirst(){
+            if(length == 0) return;
+            Node* temp = head;
+            if(length == 1) {
+                head = nullptr;
+                tail = nullptr;
+            }
+            else{
+                head = head -> next;
+            }
+            delete temp;
+            length--;
+        }
+
+        Node* get(int index){
+            if(index < 0 || index >= length){
+                return nullptr;
+            }
+            Node* temp = head;
+            for(int i = 0; i< index; i++){
+                temp = temp->next;
+            }
+            return temp;
+        }
+
+        bool set(int index, int value){
+            Node* temp = get(index);
+            if(temp) {
+                temp->value = value;
+                return true;
+            }
+            return false;
+        }
+
 };
 
 int main(){
@@ -56,7 +135,12 @@ int main(){
 
     myLinkedList -> getHead();
     myLinkedList -> getTail();
+
+    myLinkedList -> append(2);
+
     myLinkedList -> getLength();
 
-    myLinkedList -> printList();
+    myLinkedList -> deleteLast();
+
+    myLinkedList -> printList();   
 }
