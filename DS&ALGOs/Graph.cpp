f1ea7c62218dff0d32add3aada5050a224d72a91
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-02-28 21:28:55 +0530

    28-02-2023

--- DS&ALGOs/Graph.cpp
+++ DS&ALGOs/Graph.cpp
@@ -26,10 +26,37 @@
             }
             return false;
         }
+
+        bool addEdge(string vertex1, string vertex2) {
+            if(adjList.count(vertex1) != 0 && adjList.count(vertex2) != 0) {
+                adjList.at(vertex1).insert(vertex2);
+                adjList.at(vertex2).insert(vertex1);
+                return true;
+            }
+            return false;
+        }
+
+        bool removeEdge(string vertex1, string vertex2) {
+            if (adjList.count(vertex1) != 0 && adjList.count(vertex2) != 0) {
+                adjList.at(vertex1).erase(vertex2);
+                adjList.at(vertex2).erase(vertex1);
+                return true;
+            }
+            return false;
+        }
  };
 
  int main() {
     Graph* myGraph = new Graph();
     myGraph -> addVertex("A");
+    myGraph -> addVertex("B");
+    myGraph -> addVertex("C");
+
+    myGraph -> addEdge("A", "B");
+    myGraph -> addEdge("A", "C");
+    myGraph -> addEdge("B", "C");
+
+    myGraph -> removeEdge("A", "B");
+
     myGraph -> printGraph();
  }
