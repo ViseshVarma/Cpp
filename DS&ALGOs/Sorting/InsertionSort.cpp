fd5282b074056e57419b949b18e210b2d6345761
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-03-14 22:03:43 +0530

    14-03-2023

---
+++ DS&ALGOs/Sorting/InsertionSort.cpp
@@ -0,0 +1,8 @@
+/*  In insertion sort we always start with the second item
+    and if the second item is smaller then the first then we switch them,
+    if it is not smaller we wont switch them and move to the next elemets */
+
+#include <iostream>
+
+using namespace std;
+
