087ecfaf716ca6bf03b740e98c1701ed22b2e049
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-03-14 22:10:44 +0530

    14-03-2023

--- DS&ALGOs/Sorting/InsertionSort.cpp
+++ DS&ALGOs/Sorting/InsertionSort.cpp
@@ -6,3 +6,26 @@
 
 using namespace std;
 
+void insertionSort(int array[], int size){
+    for(int i=1; i<size; i++){
+        int temp = array[i];
+        int j = i - 1;
+        while( j > -1 && temp < array[j]) {
+            array[j+1] = array[j];
+            array[j] = temp;
+            j--; // refer the udemy video for visual explanation
+        }
+    }
+}
+
+
+int main() {
+    int myArray[] = {6, 4, 2, 5, 1, 3};
+    int size = sizeof(myArray) / sizeof(myArray[0]);
+
+    insertionSort(myArray, size);
+
+    for (auto value : myArray) {
+        cout << value << " ";
+    }
+}
